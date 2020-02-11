2635be8e61aa41fa758ede2916ee7e5234428bf2
Maya Liberman <mayaliberman1@gmail.com>
2020-02-11 11:44:19 +0200

    process in refactor modal

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,7 @@ function getProfiles(json) {
     const { city, state, street, postcode } = person.location;
     const birthday = person.dob.date;
     const cell = person.cell;
-   
+
     profileArr.push({
       firstName,
       lastName,
@@ -61,21 +61,39 @@ function getProfiles(json) {
 function generateHTML(arr) {
   const gallery = document.getElementById('gallery');
   arr.forEach(profile => {
+    const { picture, firstName, lastName, email, city, state } = profile;
     const card = document.createElement('div');
     card.className += 'card';
     gallery.appendChild(card);
     card.innerHTML = `
           <div class="card-img-container">
-          <img class="card-img" src=${profile.picture} alt="profile picture">
+          <img class="card-img" src=${picture} alt="profile picture">
           </div>
           <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${profile.firstName} ${profile.lastName}</h3>
-          <p class="card-text">${profile.email}</p>
-          <p class="card-text cap">${profile.city}, ${profile.state}</p>
+          <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
+          <p id="email" class="card-text">${email}</p>
+          <p class="card-text cap">${city}, ${state}</p>
           </div>
           </div>
           `;
+  });
 
+  //when a card is clicked the name of the card and the name of the modal cards are compared.
+  //if there is a match, the modal window visibility will be visibile
+
+  function createModal(profile) {
+    const {
+      picture,
+      firstName,
+      lastName,
+      email,
+      city,
+      cell,
+      street,
+      state,
+      postcode,
+      birthday
+    } = profile;
     //creation of the modal window, hide it by visibility hidden
     const modalContainer = document.createElement('div');
     modalContainer.className += 'modal-container';
@@ -83,48 +101,54 @@ function generateHTML(arr) {
       <div class="modal">
         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
           <div class="modal-info-container">
-              <img class="modal-img" src=${
-                profile.picture
-              } alt="profile picture">
-                <h3 id="name" class="modal-name cap">${profile.firstName} ${
-      profile.lastName
-    }</h3>
-                <p class="modal-text">${profile.email}</p>
-                <p class="modal-text cap">${profile.city}</p>
+              <img class="modal-img" src=${picture} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3>
+                <p class="modal-text">${email}</p>
+                <p class="modal-text cap">${city}</p>
                 <hr>
-                <p class="modal-text">${formatPhoneNumber(profile.cell)}</p>
-                <p class="modal-text cap">${profile.street}, ${profile.city}, ${
-      profile.state
-    } ,${profile.postcode}</p>
-                <p class="modal-text">Birthday: ${formatBirthday(
-                  profile.birthday
-                )}</p>
+                <p class="modal-text">${formatPhoneNumber(cell)}</p>
+                <p class="modal-text cap">${street.number} ${street.name}, ${city}, ${state} ,${postcode}</p>
+                <p class="modal-text">Birthday: ${formatBirthday(birthday)}</p>
             </div>
       </div>
       `;
-    $('#gallery').after(modalContainer);
-    $('.modal-container').css('visibility', 'hidden');
-  });
 
-  //when a card is clicked the name of the card and the name of the modal cards are compared.
-  //if there is a match, the modal window visibility will be visibile
+    modalContainer.style.display = 'block';
 
-  $('.card').on('click', e => {
-    const card = e.currentTarget;
-    const cardName = card.querySelector('.card-name').textContent;
+    const scriptJS = document.getElementsByTagName('script')[0];
+    document.body.insertBefore(modalContainer, scriptJS);
+  }
+
+  
+
+  const card = document.querySelector('.card');
+  card.addEventListener('click', e => {
+    
+    const currentCard = e.currentTarget;
+    console.log(currentCard);
+    console.log(arr);
+    const cardEmail = currentCard.querySelector('#email').textContent;
+    console.log(cardEmail)
     const cardsModal = document.querySelectorAll('.modal-container');
-    for (let i = 0; i < cardsModal.length; i++) {
-      const modalName = cardsModal[i].querySelector('#name').textContent;
-      if (modalName === cardName) {
-        cardsModal[i].style.visibility = 'visible';
+    for (let i = 0; i < arr.length; i++) {
+     
+      if(cardEmail === arr[i].email) {
+        console.log(cardEmail, arr[i].street, arr[i].picture)
+        createModal(arr[i])
       }
+      // const modalName = cardsModal[i].querySelector('#name').textContent;
+      // if (modalName === cardName) {
+      //   cardsModal[i].style.display = 'block';
+      // }
     }
   });
+
+  
 
   //When the close button is clicked the modal window visibility will be hidden again
 
   $('.modal-close-btn').on('click', () => {
-    $('.modal-container').css('visibility', 'hidden');
+    $('.modal-container').css('display', 'none');
   });
 }
 
