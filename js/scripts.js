9028008fde218815d33a0637a9da4bdbc8a38b4c
unknown <mayaliberman1@gmail.com>
2020-02-15 10:49:04 +0200

    fixed updateModule + starting nextModal

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 //  API URL
 // ------------------------------------------
 
-const peopleUrl = 'https://randomuser.me/api/?results=12&nat=us';
+const peopleUrl = "https://randomuser.me/api/?results=12&nat=us";
 
 // ------------------------------------------
 //  FETCH API
@@ -11,14 +11,14 @@ const peopleUrl = 'https://randomuser.me/api/?results=12&nat=us';
 // When the window load the fetch API is being processed
 //generating the profiles and the HTML rendering
 
-window.addEventListener('load', async () => {
+window.addEventListener("load", async () => {
   try {
     const response = await fetch(peopleUrl);
     const responseJson = await response.json();
     const profiles = getProfiles(responseJson);
     generateCards(profiles);
   } catch (err) {
-    document.write('Something went wrong');
+    document.write("Something went wrong");
     console.log(err);
   }
 });
@@ -30,11 +30,7 @@ window.addEventListener('load', async () => {
 function getProfiles(json) {
   const profileArr = [];
   json.results.forEach(person => {
-  
-    profileArr.push(
-      getProfile(person)
-    
-    );
+    profileArr.push(getSingleProfile(person));
   });
   return profileArr;
 }
@@ -45,12 +41,12 @@ function getProfiles(json) {
 
 // this function create the all the HTML elements and render it to the screen;
 function generateCards(profiles) {
-  const gallery = document.getElementById('gallery');
+  const gallery = document.getElementById("gallery");
   profiles.forEach(profile => {
     const { picture, firstName, lastName, email, city, state } = profile;
-    const card = document.createElement('div');
-    card.className += 'card';
-    card.setAttribute('data-email', email);
+    const card = document.createElement("div");
+    card.className += "card";
+    card.setAttribute("data-email", email);
     gallery.appendChild(card);
     card.innerHTML = `
           <div class="card-img-container">
@@ -68,63 +64,83 @@ function generateCards(profiles) {
 }
 
 function addModalEvents(profiles) {
-  const cards = document.querySelectorAll('.card');
+  const cards = document.querySelectorAll(".card");
   for (const card of cards) {
-    card.addEventListener('click', e => {
+    card.addEventListener("click", e => {
       const currentCard = e.currentTarget;
-      const dataEmail = currentCard.getAttribute('data-email');
-      const profile = profiles.find(item => item.email === dataEmail);
+      const dataEmail = currentCard.getAttribute("data-email");
+      const profile = profiles.find((item) => item.email === dataEmail);
       updateModal(profile);
     });
   }
 }
 
-
-//Update Modal
 function updateModal(profile) {
-  // const city = document.querySelectorAll('.modal-info-container .city').item(0).innerHTML = profile.city;
-  // console.log(city)
-  
-  for(item in profile) {
-    const child = document.querySelectorAll(`.modal-info-container .${item}`).item(0).innerHTML = item;
-  //  child.innerHTML = profile[item];
-  console.log(profile[item]);
-
+  const address = `${profile.street.number}, ${profile.street.name} ${profile.city}, ${profile.state} ${profile.postcode}`;
+  for (item in profile) {
+    if (item === "street") {
+      document
+        .querySelectorAll(`.modal-info-container .${item}`)
+        .item(0).innerHTML = address;
+    } else if (item === "picture") {
+      document.querySelector(`.modal-info-container .${item}`).src =
+        profile[item];
+    } else if (item === "postcode") {
+    } else if (item === "state") {
+    } else if (item === "lastName") {
+      document.querySelector(`.cap .${item}`).innerHTML = profile[item];
+    } else {
+      document
+        .querySelectorAll(`.modal-info-container .${item}`)
+        .item(0).innerHTML = profile[item];
+    }
   }
-  document.querySelector('.modal-container').style.display = 'block';
+  document.querySelector(".modal-container").style.display = "block";
 }
-//Closing the button
-const modalButton = document.querySelector('.modal-close-btn');
-modalButton.addEventListener('click', ( )=> {
-  document.querySelector('.modal-container').style.display = 'none';
+
+//Next Module
+function nextModule (profiles) {
+const modalNext = document.querySelector('.modal-next');
+modalNext.addEventListener('click', ()=> {
+  const modalEmail = document.querySelector('.modal-container .email');
+  const profile = profiles.findIndex((item) => item.email === modalEmail);
+  // updateModal(profile);
+  console.log(profile)
 })
 
-//document.getElementById("myImg").src = "hackanm.gif";
+}
+
+//Closing the button
+const modalButton = document.querySelector(".modal-close-btn");
+modalButton.addEventListener("click", () => {
+  document.querySelector(".modal-container").style.display = "none";
+});
+
 // --------------------------------------------------
 //  HELPER FUNCTION FOR PHONE AND BIRTHDAY FORMATTING
 // --------------------------------------------------
 
 function formatPhoneNumber(phoneNumberString) {
-  const cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+  const cleaned = ("" + phoneNumberString).replace(/\D/g, "");
   const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
   if (match) {
-    return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+    return "(" + match[1] + ") " + match[2] + "-" + match[3];
   }
   return null;
 }
 
 function formatBirthday(date) {
   const newDate = new Date(date);
-  const newEvent = newDate.toLocaleDateString('en-US');
+  const newEvent = newDate.toLocaleDateString("en-US");
   return newEvent;
 }
 
-function getProfile(profile) {
+function getSingleProfile(profile) {
   const { first: firstName, last: lastName } = profile.name;
   const picture = profile.picture.large;
   const email = profile.email;
   const { city, state, street, postcode } = profile.location;
-  const birthday = profile.dob.date;
+  const birthday = formatBirthday(profile.dob.date);
   const cell = profile.cell;
   return {
     firstName,
